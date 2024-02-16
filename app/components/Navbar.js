aaf615d4d7436671adc070678483a993fffaa4a1
Faisal Ansari <102345373+faisgit@users.noreply.github.com>
2024-02-16 17:14:06 +0530

    navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div class="navbar bg-base-100 shadow-2xl text-sm">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">Charity Finder</a>
+      </div>
+    </div>
+  );
+};
--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import { Navbar } from "./components/Navbar";
 export default function Home() {
   return (
-    <div>
-      <Link href = "/Navbar">Nav</Link>
-    </div>
+   <main>
+    <Navbar />
+   </main>
   );
 }
